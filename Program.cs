5c61d499ca545989ca06793fa06efda0a530098e
208710386 <dichavesga@est.utn.ac.cr>
2026-03-19 19:42:48 -0600

    prueba2

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace Nexora
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
